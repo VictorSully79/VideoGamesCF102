3aa624a08a3dc5ed2bcb4729447604ffeb15a9b5
VictorSully79 <victorsullivan@hotmail.com>
2021-01-07 05:48:05 -0800

    added java for name request

---
+++ cupofjoe2.js
@@ -0,0 +1,5 @@
+if (userName == 'Victor'){
+    document.write('<h3>' + 'Welcome SuperNerd Victor' + '</h3>');
+} else if (userNamee != 'Victor'){
+    document.write('<h3>' + 'Welcome Nerd' + '</h3>')
+    }
